01ca3708600e9342602a85b56525710985bdaec9
hsilvaM <hsilva@muniguate.com>
2024-11-25 11:30:26 -0600

    add full auth

--- src/permissions/permissions.controller.ts
+++ src/permissions/permissions.controller.ts
@@ -7,35 +7,45 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { CreatePermissionDto } from './dto/create-permission.dto';
 import { UpdatePermissionDto } from './dto/update-permission.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 
 @ApiTags('Permisos')
 @Controller('permissions')
 export class PermissionsController {
   constructor(private readonly permissionsService: PermissionsService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Crear un nuevo permiso' })
   @Post()
   create(@Body() createPermissionDto: CreatePermissionDto) {
     return this.permissionsService.create(createPermissionDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Listar todos los permisos' })
   @Get()
   findAll() {
     return this.permissionsService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Buscar un permiso por ID' })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: string) {
     return this.permissionsService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Actualizar un permiso' })
   @Patch(':id')
   update(
@@ -45,6 +55,8 @@ export class PermissionsController {
     return this.permissionsService.update(+id, updatePermissionDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Eliminar un permiso' })
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: string) {
--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -7,54 +7,69 @@ import {
   Param,
   Delete,
   ParseIntPipe,
+  UseGuards,
 } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
 import {
   ApiBadRequestResponse,
+  ApiBearerAuth,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
 import { AssociateRolePermissionDto } from './dto/role-permission.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 
 @ApiTags('Roles')
 @Controller('role')
 export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Crear un nuevo rol' })
   @Post()
   create(@Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(createRoleDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Añadir un permiso al rol' })
   @Post('/add-permission')
   addPermission(@Body() addPermissionDto: AssociateRolePermissionDto) {
     return this.roleService.addPermission(addPermissionDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Listar todos los roles' })
   @Get()
   findAll() {
     return this.roleService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Obtener los permisos del rol' })
   @Get('/permissions/:id')
   getPermissions(@Param('id', ParseIntPipe) id: string) {
     return this.roleService.getPermissions(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Buscar un rol por ID' })
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: string) {
     return this.roleService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Actualizar un rol' })
   @Patch(':id')
   update(
@@ -64,12 +79,16 @@ export class RoleController {
     return this.roleService.update(+id, updateRoleDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Eliminar un permiso del rol' })
   @Delete('/remove-permission')
   removePermission(@Body() removePermissionDto: AssociateRolePermissionDto) {
     return this.roleService.removePermission(removePermissionDto);
   }
 
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
   @ApiOperation({ summary: 'Eliminar un rol' })
   @Delete(':id')
   @ApiOkResponse({ description: 'Rol eliminado', type: CreateRoleDto })
